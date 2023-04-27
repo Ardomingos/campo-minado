ebbd6db4af95144507ae240fd9380db6490f1697
André Rodrigues <rodrigues.domingos996@gmail.com>
2023-04-27 14:32:30 -0300

    finalizando a logica

--- js/index.js
+++ js/index.js
@@ -1,4 +1,102 @@
-const grid = document.getElementById('grid');
+const grid = document.getElementById("grid");
 let lockGame = false;
-const testMod = false;
+// Colocar test em true para ver onde está as minas
+const testMode = false;
 generateGrid();
+
+// Gerar 10 * 10 Grid
+function generateGrid() {
+    lockGame = false;
+    grid.innerHTML = "";
+    for (var i = 0; i < 10; i++) {
+        row = grid.insertRow(i);
+        for (var j = 0; j < 10; j++) {
+            cell = row.insertCell(j);
+            cell.onclick = function () { init(this); };
+            var mine = document.createAttribute("mine");
+            mine.value = "false";
+            cell.setAttributeNode(mine);
+        }
+    }
+    generateMines();
+}
+
+// Gera minas aleatoriamente
+function generateMines() {
+    // Adiciona 20 minas no jogo
+    for (var i = 0; i < 20; i++) {
+        var row = Math.floor(Math.random() * 10);
+        var col = Math.floor(Math.random() * 10);
+        var cell = grid.rows[row].cells[col];
+        cell.setAttribute("mine", "true");
+        if (testMode) {
+            cell.innerHTML = "X";
+        }
+    }
+}
+
+// Acender todas as minas
+function revealMines() {
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            var cell = grid.rows[i].cells[j];
+            if (cell.getAttribute("mine") == "true") {
+                cell.className = "mine";
+            }
+        }
+    }
+}
+
+function checkGameComplete() {
+    var gameComplete = true;
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            if ((grid.rows[i].cells[j].getAttribute("mine") == "false") && (grid.rows[i].cells[j].innerHTML == "")) {
+                gameComplete = false;
+            }
+        }
+    }
+    if (gameComplete) {
+        alert("You Found All Mines!");
+        revealMines();
+    }
+}
+
+function init(cell) {
+    // Jogo completo ou não
+    if (lockGame) {
+        return;
+    } else {
+        // checar se o usuario clicou na mina
+        if (cell.getAttribute("mine") == "true") {
+            revealMines();
+            lockGame = true;
+        } else {
+            cell.className = "active";
+            // Mostrar o numero de celula proximas
+            var mineCount = 0;
+            var cellRow = cell.parentNode.rowIndex;
+            var cellCol = cell.cellIndex;
+            for (var i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
+                for (var j = Math.max(cellCol - 1, 0); j <= Math.min(cellCol + 1, 9); j++) {
+                    if (grid.rows[i].cells[j].getAttribute("mine") == "true") {
+                        mineCount++;
+                    }
+                }
+            }
+            cell.innerHTML = mineCount;
+            if (mineCount == 0) {
+                // Se a celula não tiver mina
+                for (var i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
+                    for (var j = Math.max(cellCol - 1, 0); j <= Math.min(cellCol + 1, 9); j++) {
+                        if (grid.rows[i].cells[j].innerHTML == "") {
+                            init(grid.rows[i].cells[j]);
+                        }
+                    }
+                }
+            }
+            checkGameComplete();
+        }
+    }
+
+}
